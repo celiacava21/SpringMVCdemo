156d947aed1dc7f64d1e8389af2fcad1f4140c84
celia <celia.cava18ruiz@gmail.com>
2023-03-21 10:35:08 +0100

    repository annotation removed

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.thymeleaf.engine.AttributeName;
+
 
 import com.example.entities.Estudiante;
 import com.example.services.EstudianteService;
