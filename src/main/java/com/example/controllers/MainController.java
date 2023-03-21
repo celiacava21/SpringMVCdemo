7dc96e19f22ac9f137c62b5eb036d22031b401ec
celia <celia.cava18ruiz@gmail.com>
2023-03-21 14:30:42 +0100

    sincronizado con la rama formulario

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -52,5 +53,12 @@ public class MainController {
         model.addAttribute("estudiante", new Estudiante());
         model.addAttribute("facultades", facultades);
         return "views/formularioAltaEstudiante";
+    }
+    /**Método que recibe los datos procedentes de los controles del formulario */
+    
+    @PostMapping("/altaEstudiante")
+    public void altaEstudiante() {
+        
+
     }
 }
